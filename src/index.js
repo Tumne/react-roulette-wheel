24727047b211e8682316b2a515e96089e1ea2e5a
Tumne <freshlygrazed@gmail.com>
2017-11-23 11:25:25 -0500

    Added in basic skeleton

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Roulette from './Roulette';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Roulette />, document.getElementById('root'));
 registerServiceWorker();
